e7c2540b5ac4a4c16fbe8e27101e349cd4ec4bc8
Ben Darlow <github@kapowaz.net>
2012-01-30 17:10:24 +0000

    Added JSONQuery for sub-selecting a path of a given data context

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,13 +2,16 @@ $(document).ready(function() {
   
   // load in the templater with a simple template for initial load-time
   var templater = new Matsuda.Templater({
-    templates: [{ name: 'home', resource: '/templates/home.mustache' }],
-    contexts:  [{ name: 'home', resource: '/json/home.json' }]
+    templates: [{ name: 'home', resource: '/templates/home.mustache' }, { name: 'subpath', resource: '/templates/subpath.mustache' }],
+    contexts:  [{ name: 'home', resource: '/json/home.json' }, { name: 'deepstructure', resource: '/json/deepstructure.json' }]
   });
   
-  // this one happens on load
-  templater.render('home', 'home', function(output){
-    $('section#container').append(output);
+  // this stuff gets handled on initial load; note the nested callbacks ensure load order
+  templater.render('home', 'home', function(html){
+    $('section#container').append(html);
+    templater.render('subpath', 'deepstructure', 'massive', function(html){
+      $('section#container').append(html);
+    });
   });
   
   // this one happens when you click the link that gets loaded dynamically
@@ -16,7 +19,7 @@ $(document).ready(function() {
     e.preventDefault();
     templater.render({name: 'postscript', resource: '/templates/postscript.mustache'}, {name: 'postscript', resource: '/json/postscript.json'}, function(output){
       $('section#container').append(output);
-    });
+    });    
   });
   
 });
--- public/javascripts/matsuda.js
+++ public/javascripts/matsuda.js
@@ -63,15 +63,30 @@ var Matsuda = {};
     // template and context can be either a name (string) of a pre-loaded template/context,
     // or an object ({name: foo, resource: '/path/to/resource'}) to be loaded
     
-    render: function(template, context, handler){
+    render: function(){
       var templater = this;
-      var loaded    = { template: false, context: false };    
+      var loaded    = { template: false, context: false };
+      
+      // pull arguments out dynamically to accommodate the optional path argument for the context
+      var template  = arguments.length >= 1 && (typeof arguments[0] == "string" || typeof arguments[0] == "object") ? arguments[0] : null;
+      var context   = arguments.length >= 2 && (typeof arguments[1] == "string" || typeof arguments[1] == "object") ? arguments[1] : null;
+      var path      = arguments.length >= 3 && typeof arguments[2] == "string" ? arguments[2] : null;
+      var handler   = null;
+      
+      if (arguments.length >= 3 && typeof arguments[2] == "function") {
+        handler = arguments[2];
+      } else if (arguments.length >= 4 && typeof arguments[3] == "function") {
+        handler = arguments[3]; 
+      }
+
+      // use a timestamp to generate a unique namespace for this particular render call
+      var namespace = "matsuda.templater.render.loaded." + new Date().getTime();
 
       // fired whenever any single load dependency completes loading
-      $(document).bind('matsuda.templater.render.loaded', function(){
+      $(document).bind(namespace, function(){
         if (typeof handler == "function" && loaded.template && loaded.context) {
           handler(loaded.template.render(loaded.context));
-          $(document).unbind('matsuda.templater.render.loaded');
+          $(document).unbind(namespace);
         }
       });
       
@@ -79,37 +94,37 @@ var Matsuda = {};
       if (typeof template == "string") {
         if (typeof templater.templates[template] != "undefined") {
           loaded.template = templater.templates[template];
-          $(document).trigger('matsuda.templater.render.loaded', ['template: pre-existing,  loaded']);
+          $(document).trigger(namespace, ['template: pre-existing,  loaded']);
         } else {
           $(document).bind('matsuda.templater.template.loaded.' + template, function(){
             loaded.template = templater.templates[template];
-            $(document).trigger('matsuda.templater.render.loaded', ['template: pre-existing, delayed']);
+            $(document).trigger(namespace, ['template: pre-existing, delayed']);
           });
         }
       } else if (typeof template == "object") {
         // we're loading in a fresh template that hasn't previously been loaded 
         templater.loadTemplate('template', template.name, template.resource, function(){
           loaded.template = templater.templates[template.name];
-          $(document).trigger('matsuda.templater.render.loaded', ['template: not pre-existing']);
+          $(document).trigger(namespace, ['template: not pre-existing']);
         });
       }
       
       // 2.) The Context
       if (typeof context == "string") {
         if (typeof templater.contexts[context] != "undefined") {
-          loaded.context = template.contexts[context];
-          $(document).trigger('matsuda.templater.render.loaded', ['context: pre-existing, loaded']);
+          loaded.context = (path == null ? templater.contexts[context] : JSONQuery(path, templater.contexts[context]));
+          $(document).trigger(namespace, ['context: pre-existing, loaded']);
         } else {
           $(document).bind('matsuda.templater.context.loaded.' + context, function(){
-            loaded.context = templater.contexts[context];
-            $(document).trigger('matsuda.templater.render.loaded', ['context: pre-existing, delayed']);
+            loaded.context = (path == null ? templater.contexts[context] : JSONQuery(path, templater.contexts[context]));
+            $(document).trigger(namespace, ['context: pre-existing, delayed']);
           });
         }
       } else if (typeof context == "object") {
         // we're loading in a fresh context that hasn't previously been loaded
         templater.loadContext(context.name, context.resource, function(){
-          loaded.context = templater.contexts[context.name];
-          $(document).trigger('matsuda.templater.render.loaded', ['context: not pre-existing']);
+          loaded.context = (path == null ? templater.contexts[context.name] : JSONQuery(path, templater.contexts[context.name]));
+          $(document).trigger(namespace, ['context: not pre-existing']);
         });
       }
     } // Matsuda.Templater.render()
